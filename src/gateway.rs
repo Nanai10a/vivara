8c75084670eff12ad3e1458d007f4e66d0d24fdf
Nanai10a <Nanai10a@outlook.jp>
2021-09-25 02:44:22 +0900

    🚧 implemeneted about `Gateway`.

--- src/gateway.rs
+++ src/gateway.rs
@@ -1,4 +1,10 @@
 use actix::prelude::*;
+use futures_util::StreamExt;
+use twilight_gateway::{Cluster, Intents};
+use twilight_model::gateway::event::Event;
+
+use crate::cmd::CommandParser;
+use crate::util::{token, Pipe};
 
 #[derive(Debug)]
 pub struct MsgRef {
@@ -39,3 +45,72 @@ impl Message for Reply {
 }
 
 pub fn get_reply_recipient() -> Recipient<Reply> { unimplemented!() }
+
+#[derive(Default)]
+struct Gateway;
+impl Actor for Gateway {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        let addr = ctx.address();
+
+        async move {
+            let (clu, mut eve) = Cluster::new(
+                token::<String>(),
+                Intents::GUILD_MESSAGES | Intents::DIRECT_MESSAGES,
+            )
+            .await
+            .unwrap();
+
+            clu.up().await;
+
+            while let Some((_, e)) = eve.next().await {
+                if let Event::MessageCreate(mc) = e {
+                    GatewayMessage {
+                        content: mc.0.content,
+                        from: MsgRef {
+                            message: mc.0.id.0,
+                            channel: mc.0.channel_id.0,
+                            guild: mc.0.guild_id.map(|i| i.0),
+                        },
+                        user: mc.0.author.id.0,
+                    }
+                    .pipe(|m| addr.do_send(m));
+                }
+            }
+        }
+        .into_actor(self)
+        .spawn(ctx)
+    }
+}
+impl Handler<GatewayMessage> for Gateway {
+    type Result = ();
+
+    fn handle(
+        &mut self,
+        GatewayMessage {
+            content,
+            from,
+            user,
+        }: GatewayMessage,
+        _: &mut Self::Context,
+    ) -> Self::Result {
+        CommandParser::from_registry().do_send(RawCommand {
+            content,
+            from,
+            user,
+        })
+    }
+}
+impl Supervised for Gateway {}
+impl ArbiterService for Gateway {}
+
+struct GatewayMessage {
+    content: String,
+    from: MsgRef,
+    user: u64,
+}
+
+impl Message for GatewayMessage {
+    type Result = ();
+}
