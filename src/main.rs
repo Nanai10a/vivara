df2f3a5ea6e0acabfbedc95eb88a5d3cd728bb2c
Nanai Jua <me@n7i.dev>
2022-09-12 18:31:10 +0900

    🐛 refactor and change codes for newer APIs.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,18 @@ mod connection;
 mod gateway;
 mod util;
 
+use alloc::sync::Arc;
+
+use actix::Registry;
+use connection::Connector;
+use gateway::{Gateway, GatewayMessage, MessageRef};
+use songbird::Songbird;
+use twilight_gateway::cluster::Events;
+use twilight_gateway::{Cluster, Event, Intents};
+use twilight_http::Client;
+use twilight_model::id::marker::UserMarker;
+use twilight_model::id::Id;
+
 fn main() {
     tracing_subscriber::fmt().pretty().init();
 
@@ -29,20 +41,14 @@ fn main() {
 }
 
 async fn initialize() {
-    use alloc::sync::Arc;
-
-    use actix::{Actor, ArbiterService, Registry};
-    use connection::Connector;
+    use actix::{Actor, ArbiterService};
     use futures_util::StreamExt;
-    use gateway::{Gateway, GatewayMessage, MessageRef};
-    use songbird::Songbird;
-    use twilight_gateway::Event;
     use util::Pipe;
 
     let (cluster, user_id, mut events) = build_cluster().await;
     cluster.up().await;
 
-    let songbird = Songbird::twilight(cluster, user_id).pipe(Arc::new);
+    let songbird = Songbird::twilight(Arc::new(cluster), user_id).pipe(Arc::new);
 
     let connector = Connector::new(songbird.clone()).start();
     Registry::set(connector);
@@ -58,11 +64,11 @@ async fn initialize() {
                 let msg = GatewayMessage {
                     content: mc.0.content,
                     from: MessageRef {
-                        message: mc.0.id.0,
-                        channel: mc.0.channel_id.0,
+                        message: mc.0.id.get(),
+                        channel: mc.0.channel_id.get(),
                     },
-                    user: mc.0.author.id.0,
-                    guild: mc.0.guild_id.map(|i| i.0),
+                    user: mc.0.author.id.get(),
+                    guild: mc.0.guild_id.map(|i| i.get()),
                 };
 
                 gateway.try_send(msg).expect("failed sending")
@@ -73,19 +79,14 @@ async fn initialize() {
     tokio::spawn(fut);
 }
 
-async fn build_cluster() -> (
-    twilight_gateway::Cluster,
-    twilight_model::id::UserId,
-    twilight_gateway::cluster::Events,
-) {
-    use twilight_gateway::{Cluster, Intents};
-    use twilight_http::Client;
-    use util::token;
-
+async fn build_cluster() -> (Cluster, Id<UserMarker>, Events) {
     let (cluster, events) = loop {
         match Cluster::new(
-            token::<String>(),
-            Intents::GUILD_VOICE_STATES | Intents::GUILD_MESSAGES | Intents::DIRECT_MESSAGES,
+            util::token::<String>(),
+            Intents::GUILD_VOICE_STATES
+                | Intents::GUILD_MESSAGES
+                | Intents::DIRECT_MESSAGES
+                | Intents::MESSAGE_CONTENT,
         )
         .await
         {
@@ -95,7 +96,7 @@ async fn build_cluster() -> (
     };
 
     let user_id = loop {
-        let result = Client::new(token()).current_user().exec().await;
+        let result = Client::new(util::token()).current_user().exec().await;
 
         let response = match result {
             Ok(o) => o,
